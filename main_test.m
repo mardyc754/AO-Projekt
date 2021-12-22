b4d56a05008164648f9cdd8bfeb132b355ec6b5e
mdmkl6 <mdmkl1gim@gmail.com>
2021-12-22 14:16:00 +0100

    neural_network_v1

--- main_test.m
+++ main_test.m
@@ -1,12 +1,19 @@
 clear;
 clc;
-im=mbinarize(imread('t1.jpg'));
-leters=separate_lines_letters(im);
-p1=1;
+im=mbinarize(imread('lern.jpg'));
+leters=separate_lines_letters(im); 
 n=size(leters);
 n=n(2);
-for i=1:n
-    subplot(6,floor(n/5),i);
-    imshow(leters{i});
+% for i=1:n
+%     subplot(11,floor(n/10),i);
+%     imshow(leters{i});
+% end
 
-end
+[in,out]=net_data_to_train();
+
+nn=feedforwardnet(100);
+nn=train(nn,in,out);
+
+a=nn(object_wsp(leters{1,1})');
+a=abs(1-a);
+[M,I] = min(a);
